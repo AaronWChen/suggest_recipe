dcd7f1b062087e811182ae26c1929f5c699f4b3d
Aaron W Chen <awchen2009@gmail.com>
2019-10-28 09:43:05 -0700

    Pause work with food.com

---
+++ python/edamam_api_req.py
@@ -0,0 +1,54 @@
+""" This Python file calls the Edamam API to return a JSON file with desired
+information.
+
+Test V1 is just to send a simple request for 5 carnitas tacos recipes.
+"""
+
+import requests
+import json
+import csv
+
+api_base = "https://api.edamam.com/search?"
+
+search_q = input("What are you looking for? ")
+# Implement lemmatization using trained dataset on input in order to make 
+# future database be less likely to have redundant entries 
+# (e.g., taco vs tacos)
+
+q = f"q={search_q}"
+
+# Level up:
+# Check a database of dishes to see if this query has been asked for already
+# If not, do an API call
+
+# Currently, just does an API call, may hit API limit if continuing with this
+# version
+with open("../secrets/edamam.json","r") as f:
+  cred = json.load(f)
+
+app_id = cred["id"]
+app_id_s = f"&app_id=${app_id}"
+
+app_key = cred["key"]
+app_key_s = f"&app_key=${app_key}"
+
+# Level up: call for a few recipes using limiter and make an "average version"
+# of the input in order to get better results from the API call
+# limiter = "&from=0&to=4"
+
+api_call = api_base + q+ app_id_s + app_key_s #+ limiter
+
+#print(api_call)
+
+resp = requests.get(api_call)
+
+if resp.status_code == 200:
+    response_dict = resp.json()
+    resp_dict_hits = response_dict['hits']
+    count = 0
+    with open("../write_data/test_api_hits_recipe.json", "w") as f:
+      json.dump(resp_dict_hits['recipe'], f)
+
+else:
+  print("Error, unable to retrieve. Server response code is: ", 
+        resp.status_code)
